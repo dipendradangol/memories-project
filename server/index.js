609f4d05fc62a55a9d5b7c4fb9ef5e9abb26b279
dipendradangol <dipendra.dangol11@gmail.com>
2020-11-13 17:37:45 +0200

    server connecting to database and creating route for backend application

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+// express middleware to connect the application
+app.use('/posts', postRoutes); //every routes inside the postRoutes starts with posts
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
